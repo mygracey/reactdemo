79ce31c9f8fb8e15baa96810834ddf0a9befa1fd
grace <graceogwe@gmail.com>
2024-05-16 14:02:52 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App(){
+  return(
+    <div className='app'>
+      <h1 style={{color:"dodgerblue",textAlign:"center",position:"relative",left:"200px"}}>Welcome to React App Tutorial</h1>
+    </div>
+  )
+}
